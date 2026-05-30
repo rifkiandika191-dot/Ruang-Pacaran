b7cf27a7836687cebc49659a0b40de80b3d82494
Muhammad Rifqi <rifkiandika191@gmail.com>
2026-05-31 00:19:07 +0800

    auto: 2026-05-31 00:19:07

--- public/js/donation-effect.js
+++ public/js/donation-effect.js
@@ -199,7 +199,7 @@
       <div class="dt-body">
         <div class="dt-who"  id="dtWho"></div>
         <div class="dt-amt"  id="dtAmt"></div>
-        <div class="dt-hint">🎆 min. Rp 10rb = efek untuk semua!</div>
+        <div class="dt-hint">🎆 min. Rp 3rb = efek untuk semua!</div>
       </div>
       <a href="${SAWERIA}" target="_blank" rel="noopener" class="dt-btn">☕ Donasi</a>
       <button class="dt-close" id="dtClose">✕</button>
